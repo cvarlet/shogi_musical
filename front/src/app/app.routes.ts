a850f5c1230defeb69cec23e622625d402c9c2ce
cvarlet <varlet.charlotte@gmail.com>
2026-03-25 17:21:16 +0100

    layout

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Shogiban } from './shogiban/shogiban';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'analyse',
+  },
+  {
+    path: 'analyse',
+    component: Shogiban,
+  },
+  {
+    path: '**',
+    redirectTo: 'analyse',
+  },
+];
--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { Shogiban } from './shogiban/shogiban';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Shogiban],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = 'ShogiBase';
-}
+export class App {}
